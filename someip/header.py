36144fa2c24ae1c6b88c971e0d40e858bb2dd63f
Kjell Braden <afflux@pentabarf.de>
2020-03-14 14:44:33 +0100

    header: removed redundant code from SOMEIPHeader
    - raise ParseError if payloads are too short to parse
    - raise ParseError if option indexes on entries are out of range
    - remove DefaultEnum, raise ParseErrors if unknown codes are used

--- someip/header.py
+++ someip/header.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 
 import asyncio
 import copy
@@ -24,63 +25,7 @@ class IncompleteReadError(ParseError):
     pass
 
 
-class DefaultEnum(enum.Enum):
-    '''
-    when an undefined value is retrieved, a pseudo member is created to represent that value.
-    assign a % format string to __default_name__ to get a string representation of undefined values.
-
-    if assigning a value to multiple names, the first name becomes the representation of the value,
-    while the other names become aliases to the first name.
-    if assigning to multiple names in a single statement, the leftmost name is considered the first
-    name.
-
-    requires python3.6+.
-
-    example:
-
-    class Foo(int, DefaultEnum):
-        __default_name__ = 'RESERVED_0x%02x'
-        VALUE_A = ALIAS_A = 0
-        VALUE_B = 1
-
-    >>> Foo(0)
-    <Foo.VALUE_A: 0>
-
-    >>> Foo(1)
-    <Foo.VALUE_B: 1>
-
-    >>> Foo(2)
-    <Foo.RESERVED_0x02: 2>
-
-    >>> Foo.ALIAS_A
-    <Foo.VALUE_A: 0>
-    '''
-
-    __default_name__: typing.Optional[str] = None
-    @classmethod
-    def _missing_(cls, value):
-        if not isinstance(value, int):
-            raise ValueError('%r is not a valid %s' % (value, cls.__name__))
-        new_member = cls._create_pseudo_member_(value)
-        return new_member
-
-    @classmethod
-    def _create_pseudo_member_(cls, value):
-        # construct singleton pseudo-members
-        pseudo_member = int.__new__(cls, value)
-        # pylint: disable=protected-access,attribute-defined-outside-init
-        pseudo_member._name_ = cls.__default_name__
-        if pseudo_member._name_:
-            pseudo_member._name_ %= value
-        pseudo_member._value_ = value
-        # pylint: enable=protected-access,attribute-defined-outside-init
-        # use setdefault in case another thread already created a composite
-        # with this value
-        return cls._value2member_map_.setdefault(value, pseudo_member)
-
-
-class SOMEIPMessageType(DefaultEnum):
-    __default_name__ = 'UNKNOWN_0x%02x'
+class SOMEIPMessageType(enum.IntEnum):
     REQUEST = 0
     REQUEST_NO_RETURN = 1
     NOTIFICATION = 2
@@ -93,8 +38,7 @@ class SOMEIPMessageType(DefaultEnum):
     ERROR_ACK = 0xc1
 
 
-class SOMEIPReturnCode(DefaultEnum):
-    __default_name__ = 'UNKNOWN_0x%02x'
+class SOMEIPReturnCode(enum.IntEnum):
     E_OK = 0
     E_NOT_OK = 1
     E_UNKNOWN_SERVICE = 2
@@ -128,7 +72,7 @@ class SOMEIPHeader:
     payload: bytes = dataclasses.field(default=b'')
 
     @property
-    def description(self):
+    def description(self):  # pragma: nocover
         return f'''service: 0x{self.service_id:04x}
 method: 0x{self.method_id:04x}
 client: 0x{self.client_id:04x}
@@ -139,7 +83,7 @@ message: {self.message_type.name}
 return code: {self.return_code.name}
 payload: {len(self.payload)} bytes'''
 
-    def __str__(self):
+    def __str__(self):  # pragma: nocover
         return f'service=0x{self.service_id:04x}, method=0x{self.method_id:04x},' \
                f' client=0x{self.client_id:04x}, session=0x{self.session_id:04x},' \
                f' protocol={self.protocol_version}, interface=0x{self.interface_version:02x},' \
@@ -147,45 +91,51 @@ payload: {len(self.payload)} bytes'''
                f' payload: {len(self.payload)} bytes'
 
     @classmethod
-    def parse(cls, buf: bytes) -> typing.Tuple['SOMEIPHeader', bytes]:
+    def _parse_header(cls, parsed) -> typing.Tuple[int, typing.Callable[[bytes], SOMEIPHeader]]:
+        sid, mid, size, cid, sessid, pv, iv, mt_b, rc_b = parsed
+        if pv != 1:
+            raise ParseError(f'bad someip protocol version 0x{pv:02x}, expected 0x01')
+
+        try:
+            mt = SOMEIPMessageType(mt_b)
+        except ValueError as exc:
+            raise ParseError('bad someip message type {mt_b:#x}') from exc
+        try:
+            rc = SOMEIPReturnCode(rc_b)
+        except ValueError as exc:
+            raise ParseError('bad someip return code {rc_b:#x}') from exc
+
+        return size, lambda payload_b: cls(service_id=sid, method_id=mid, client_id=cid,
+                                           session_id=sessid, protocol_version=pv,
+                                           interface_version=iv, message_type=mt, return_code=rc,
+                                           payload=payload_b)
+
+    @classmethod
+    def parse(cls, buf: bytes) -> typing.Tuple[SOMEIPHeader, bytes]:
         '''
         parses SOMEIP packet in buffer, returns tuple (S, B)
         where S is parsed SOMEIPHeader including payload
         and B is unparsed rest of buffer
         '''
-        (sid, mid, size, cid, sessid, pv, iv, mt_b, rc_b), buf_rest = unpack(cls.__format, buf)
-        if pv != 1:
-            raise ParseError(f'bad someip protocol version 0x{pv:02x}, expected 0x01')
+        parsed, buf_rest = unpack(cls.__format, buf)
+        size, builder = cls._parse_header(parsed)
         if len(buf_rest) < size - 8:
             raise IncompleteReadError(f'packet too short, expected {size+4}, got {len(buf)}')
         payload_b, buf_rest = buf_rest[:size-8], buf_rest[size-8:]
 
-        mt = SOMEIPMessageType(mt_b)
-        rc = SOMEIPReturnCode(rc_b)
-
-        parsed = cls(service_id=sid, method_id=mid, client_id=cid, session_id=sessid,
-                     protocol_version=pv, interface_version=iv, message_type=mt, return_code=rc,
-                     payload=payload_b)
+        parsed = builder(payload_b)
 
         return parsed, buf_rest
 
     @classmethod
-    async def read(cls, buf: asyncio.StreamReader) -> 'SOMEIPHeader':
+    async def read(cls, buf: asyncio.StreamReader) -> SOMEIPHeader:
         hdr_b = await buf.readexactly(cls.__format.size)
-        sid, mid, size, cid, sessid, pv, iv, mt_b, rc_b = cls.__format.unpack(hdr_b)
-        if pv != 1:
-            raise ParseError(f'bad someip protocol version 0x{pv:02x}, expected 0x01')
+        parsed = cls.__format.unpack(hdr_b)
+        size, builder = cls._parse_header(parsed)
 
         payload_b = await buf.readexactly(size-8)
 
-        mt = SOMEIPMessageType(mt_b)
-        rc = SOMEIPReturnCode(rc_b)
-
-        parsed = cls(service_id=sid, method_id=mid, client_id=cid, session_id=sessid,
-                     protocol_version=pv, interface_version=iv, message_type=mt, return_code=rc,
-                     payload=payload_b)
-
-        return parsed
+        return builder(payload_b)
 
     def build(self) -> bytes:
         size = len(self.payload) + 8
@@ -206,8 +156,7 @@ class SOMEIPReader:
         return self.reader.at_eof()
 
 
-class SOMEIPSDEntryType(DefaultEnum):
-    __default_name__ = 'UNKNOWN_0x%02x'
+class SOMEIPSDEntryType(enum.IntEnum):
     FindService = 0
     OfferService = 1
     Subscribe = 6
@@ -249,14 +198,14 @@ class SOMEIPSDEntry:
     ttl: int
     minver_or_counter: int
 
-    options_1: typing.List['SOMEIPSDOption'] = dataclasses.field(default_factory=list)
-    options_2: typing.List['SOMEIPSDOption'] = dataclasses.field(default_factory=list)
+    options_1: typing.List[SOMEIPSDOption] = dataclasses.field(default_factory=list)
+    options_2: typing.List[SOMEIPSDOption] = dataclasses.field(default_factory=list)
     option_index_1: typing.Optional[int] = dataclasses.field(default=None)
     option_index_2: typing.Optional[int] = dataclasses.field(default=None)
     num_options_1: typing.Optional[int] = dataclasses.field(default=None)
     num_options_2: typing.Optional[int] = dataclasses.field(default=None)
 
-    def __str__(self) -> str:
+    def __str__(self) -> str:  # pragma: nocover
         if self.sd_type in (SOMEIPSDEntryType.FindService, SOMEIPSDEntryType.OfferService):
             version = f'{self.major_version}.{self.service_minor_version}'
         elif self.sd_type in (SOMEIPSDEntryType.Subscribe, SOMEIPSDEntryType.SubscribeAck):
@@ -286,7 +235,7 @@ class SOMEIPSDEntry:
         return self.option_index_1 is None or self.option_index_2 is None \
                 or self.num_options_1 is None or self.num_options_2 is None
 
-    def resolve_options(self, options: typing.Sequence['SOMEIPSDOption']):
+    def resolve_options(self, options: typing.Sequence[SOMEIPSDOption]):
         if self.options_resolved:
             raise ValueError('options already resolved')
 
@@ -312,7 +261,7 @@ class SOMEIPSDEntry:
             hdr_options.extend(entry_options)
         return oi, no
 
-    def assign_option_index(self, options: typing.List['SOMEIPSDOption']):
+    def assign_option_index(self, options: typing.List[SOMEIPSDOption]):
         if not self.options_resolved:
             return
 
@@ -338,13 +287,23 @@ class SOMEIPSDEntry:
         return self.minver_or_counter & 0xffff
 
     @classmethod
-    def parse(cls, buf: bytes) -> typing.Tuple['SOMEIPSDEntry', bytes]:
+    def parse(cls, buf: bytes, num_options: int) -> typing.Tuple[SOMEIPSDEntry, bytes]:
         (sd_type_b, oi1, oi2, numopt, sid, iid, majv, ttl_hi, ttl_lo, val), buf_rest \
                 = unpack(cls.__format, buf)
-        sd_type = SOMEIPSDEntryType(sd_type_b)
+        try:
+            sd_type = SOMEIPSDEntryType(sd_type_b)
+        except ValueError as exc:
+            raise ParseError('bad someip sd entry type {sd_type_b:#x}') from exc
+
         no1 = numopt >> 4
         no2 = numopt & 0x0f
         ttl = (ttl_hi << 16) | ttl_lo
+
+        if oi1 + no1 > num_options:
+            raise ParseError(f'SD entry options_1 ({oi1}:{oi1+no1}) out of range ({num_options})')
+
+        if oi2 + no2 > num_options:
+            raise ParseError(f'SD entry options_2 ({oi2}:{oi2+no2}) out of range ({num_options})')
 
         if sd_type in (SOMEIPSDEntryType.Subscribe, SOMEIPSDEntryType.SubscribeAck):
             if val & 0xfff00000:
@@ -369,16 +328,16 @@ class SOMEIPSDEntry:
 
 class SOMEIPSDOption:
     __format: typing.ClassVar[struct.Struct] = struct.Struct('!HB')
-    options: typing.Dict[int, typing.Type['SOMEIPSDAbstractOption']] = {}
+    options: typing.Dict[int, typing.Type[SOMEIPSDAbstractOption]] = {}
 
     @classmethod
-    def register(cls, option_cls: typing.Type['SOMEIPSDAbstractOption']) \
-            -> typing.Type['SOMEIPSDAbstractOption']:
+    def register(cls, option_cls: typing.Type[SOMEIPSDAbstractOption]) \
+            -> typing.Type[SOMEIPSDAbstractOption]:
         cls.options[option_cls.type_] = option_cls
         return option_cls
 
     @classmethod
-    def parse(cls, buf: bytes) -> typing.Tuple['SOMEIPSDOption', bytes]:
+    def parse(cls, buf: bytes) -> typing.Tuple[SOMEIPSDOption, bytes]:
         (len_b, type_b), buf_rest = unpack(cls.__format, buf)
         opt_b, buf_rest = buf_rest[:len_b], buf_rest[len_b:]
 
@@ -407,7 +366,7 @@ class SOMEIPSDAbstractOption(SOMEIPSDOption):
     type_: typing.ClassVar[int]
 
     @classmethod
-    def parse_option(cls, buf: bytes) -> 'SOMEIPSDAbstractOption': ...
+    def parse_option(cls, buf: bytes) -> SOMEIPSDAbstractOption: ...
 
 
 @SOMEIPSDOption.register
@@ -418,7 +377,7 @@ class SOMEIPSDLoadBalancingOption(SOMEIPSDAbstractOption):
     weight: int
 
     @classmethod
-    def parse_option(cls, buf: bytes) -> 'SOMEIPSDLoadBalancingOption':
+    def parse_option(cls, buf: bytes) -> SOMEIPSDLoadBalancingOption:
         if len(buf) != 5:
             raise ParseError(f'SD load balancing option with wrong payload length {len(buf)} != 5')
 
@@ -436,7 +395,7 @@ class SOMEIPSDConfigOption(SOMEIPSDAbstractOption):
     configs: typing.Sequence[typing.Tuple[str, typing.Optional[str]]]
 
     @classmethod
-    def parse_option(cls, buf: bytes) -> 'SOMEIPSDConfigOption':
+    def parse_option(cls, buf: bytes) -> SOMEIPSDConfigOption:
         if len(buf) < 2:
             raise ParseError(f'SD config option with wrong payload length {len(buf)} < 2')
 
@@ -476,8 +435,7 @@ class SOMEIPSDConfigOption(SOMEIPSDAbstractOption):
         return self.build_option(self.type_, buf)
 
 
-class L4Protocols(DefaultEnum):
-    __default_name__ = 'UNKNOWN_0x%02x'
+class L4Protocols(enum.IntEnum):
     TCP = socket.IPPROTO_TCP
     UDP = socket.IPPROTO_UDP
 
@@ -487,23 +445,26 @@ class AbstractIPOption(SOMEIPSDAbstractOption):
     _format: typing.ClassVar[struct.Struct]
     _address_type: typing.ClassVar[typing.Type]
     address: typing.Union[ipaddress.IPv4Address, ipaddress.IPv6Address]
-    l4proto: L4Protocols
+    l4proto: typing.Union[L4Protocols, int]
     port: int
 
     @classmethod
-    def parse_option(cls, buf: bytes) -> 'AbstractIPOption':
+    def parse_option(cls, buf: bytes) -> AbstractIPOption:
         if len(buf) != cls._format.size:
             raise ParseError(f'{cls.__name__} with wrong payload length {len(buf)} != 9')
 
         r1, addr_b, r2, l4proto_b, port = cls._format.unpack(buf)
 
         addr = cls._address_type(addr_b)
-        l4proto = L4Protocols(l4proto_b)
+        try:
+            l4proto = L4Protocols(l4proto_b)
+        except ValueError:
+            l4proto = l4proto_b
 
         return cls(address=addr, l4proto=l4proto, port=port)
 
     def build(self) -> bytes:
-        payload = self._format.pack(0, self.address.packed, 0, self.l4proto.value, self.port)
+        payload = self._format.pack(0, self.address.packed, 0, self.l4proto, self.port)
         return self.build_option(self.type_, payload)
 
 
@@ -523,7 +484,7 @@ class AbstractIPv4Option(AbstractIPOption):
     _format: typing.ClassVar[struct.Struct] = struct.Struct('!B4sBBH')
     _address_type = ipaddress.IPv4Address
 
-    def __str__(self) -> str:
+    def __str__(self) -> str:  # pragma: nocover
         return f'{self.address}:{self.port} ({self.l4proto.name})'
 
 
@@ -531,7 +492,7 @@ class AbstractIPv6Option(AbstractIPOption):
     _format: typing.ClassVar[struct.Struct] = struct.Struct('!B16sBBH')
     _address_type = ipaddress.IPv6Address
 
-    def __str__(self) -> str:
+    def __str__(self) -> str:  # pragma: nocover
         return f'[{self.address}]:{self.port} ({self.l4proto.name})'
 
 
@@ -583,40 +544,40 @@ class SOMEIPSDHeader:
         for e in self.entries:
             e.assign_option_index(self.options)
 
-    def __str__(self):
+    def __str__(self):  # pragma: nocover
         entries = '\n'.join(str(e) for e in self.entries)
         return f'reboot={self.flag_reboot}, unicast={self.flag_unicast}, entries:\n{entries}'
 
     @classmethod
-    def parse(cls, buf: bytes) -> typing.Tuple['SOMEIPSDHeader', bytes]:
+    def parse(cls, buf: bytes) -> typing.Tuple[SOMEIPSDHeader, bytes]:
         if len(buf) < 12:
-            raise IncompleteReadError(f'can not parse SOMEIPSDHeader, got only {len(buf)} bytes')
+            raise ParseError(f'can not parse SOMEIPSDHeader, got only {len(buf)} bytes')
 
         flags = buf[0]
 
         entries_length = struct.unpack('!I', buf[4:8])[0]
         rest_buf = buf[8:]
         if len(rest_buf) < entries_length + 4:
-            raise IncompleteReadError(f'can not parse SOMEIPSDHeader, entries length too big'
-                                      f' ({entries_length})')
+            raise ParseError(f'can not parse SOMEIPSDHeader, entries length too big'
+                             f' ({entries_length})')
         entries_buffer, rest_buf = rest_buf[:entries_length], rest_buf[entries_length:]
 
         options_length = struct.unpack('!I', rest_buf[:4])[0]
         rest_buf = rest_buf[4:]
         if len(rest_buf) < options_length:
-            raise IncompleteReadError(f'can not parse SOMEIPSDHeader, options length too big'
-                                      f' ({options_length}')
+            raise ParseError(f'can not parse SOMEIPSDHeader, options length too big'
+                             f' ({options_length}')
         options_buffer, rest_buf = rest_buf[:options_length], rest_buf[options_length:]
-
-        entries = []
-        while entries_buffer:
-            entry, entries_buffer = SOMEIPSDEntry.parse(entries_buffer)
-            entries.append(entry)
 
         options = []
         while options_buffer:
             option, options_buffer = SOMEIPSDOption.parse(options_buffer)
             options.append(option)
+
+        entries = []
+        while entries_buffer:
+            entry, entries_buffer = SOMEIPSDEntry.parse(entries_buffer, len(options))
+            entries.append(entry)
 
         flag_reboot = bool(flags & 0x80)
         flags &= ~0x80
