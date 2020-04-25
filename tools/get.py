30d09a26ffde5b24e5b36db75dc4df3cae4a8c18
Kjell Braden <afflux@pentabarf.de>
2020-04-25 18:56:04 +0200

    tools/get.py: adapt to new API

--- tools/get.py
+++ tools/get.py
@@ -1,46 +1,45 @@
 #!/usr/bin/env python3
+import argparse
 import asyncio
 import ipaddress
 import logging
 import typing
 
 import someip.header
-from someip.sd import DatagramProtocol
+from someip.sd import SOMEIPDatagramProtocol
 
-LOG = logging.getLogger('someip.get')
+LOG = logging.getLogger("someip.get")
 
 
-class Prot(DatagramProtocol):
-    def get(self, service, method):
+class Prot(SOMEIPDatagramProtocol):
+    def get(self, service, method, major_version):
         hdr = someip.header.SOMEIPHeader(
             service_id=service,
             method_id=method,
             client_id=0,
             session_id=0,
-            interface_version=1,
+            interface_version=major_version,
             message_type=someip.header.SOMEIPMessageType.REQUEST,
         )
-        self.transport.sendto(hdr.build())
+        self.send(hdr.build())
 
-    def message_received(self,
-                         someip_message: someip.header.SOMEIPHeader,
-                         addr: typing.Tuple[str, int]) -> None:
-        '''
-        called when a well-formed SOME/IP datagram was received
-        '''
-        super().message_received(someip_message, addr)
-        LOG.info('payload: %r', someip_message.payload)
+    def message_received(
+        self,
+        someip_message: someip.header.SOMEIPHeader,
+        addr: typing.Tuple[str, int],
+        multicast: bool,
+    ) -> None:
+        LOG.info(
+            "response: %r / %r", someip_message.return_code, someip_message.payload
+        )
 
 
-async def run(addr, port, service, method, interval):
-    transport, protocol = await asyncio.get_event_loop().create_datagram_endpoint(
-        Prot,
-        remote_addr=(str(addr), port),
-    )
+async def run(addr, port, service, method, version, interval):
+    transport, prot = await Prot.create_unicast_endpoint(remote_addr=(str(addr), port))
 
     try:
         while True:
-            protocol.get(service, method)
+            prot.get(service, method, version)
             await asyncio.sleep(interval)
     except asyncio.CancelledError:
         pass
@@ -52,38 +51,43 @@ def auto_int(s):
     return int(s, 0)
 
 
-def setup_log(fmt='', **kwargs):
+def setup_log(fmt="", **kwargs):
     try:
         import coloredlogs
-        coloredlogs.install(fmt='%(asctime)s,%(msecs)03d ' + fmt, **kwargs)
+
+        coloredlogs.install(fmt="%(asctime)s,%(msecs)03d " + fmt, **kwargs)
     except ModuleNotFoundError:
-        logging.basicConfig(format='%(asctime)s ' + fmt, **kwargs)
-        logging.info('install coloredlogs for colored logs :-)')
+        logging.basicConfig(format="%(asctime)s " + fmt, **kwargs)
+        logging.info("install coloredlogs for colored logs :-)")
 
 
 def main():
-    setup_log('%(levelname)-8s %(name)s: %(message)s', level=logging.DEBUG)
-
-    import argparse
+    setup_log("%(levelname)-8s %(name)s: %(message)s", level=logging.DEBUG)
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('host', type=ipaddress.ip_address)
-    parser.add_argument('port', type=int)
-    parser.add_argument('service', type=auto_int)
-    parser.add_argument('method', type=auto_int)
-    parser.add_argument('--interval', type=int, default=3)
+    parser.add_argument("host", type=ipaddress.ip_address)
+    parser.add_argument("port", type=int)
+    parser.add_argument("service", type=auto_int)
+    parser.add_argument("method", type=auto_int)
+    parser.add_argument("version", type=auto_int)
+    parser.add_argument("--interval", type=int, default=3)
 
     args = parser.parse_args()
 
     try:
-        asyncio.get_event_loop().run_until_complete(run(args.host,
-                                                        args.port,
-                                                        args.service,
-                                                        args.method,
-                                                        args.interval))
+        asyncio.get_event_loop().run_until_complete(
+            run(
+                args.host,
+                args.port,
+                args.service,
+                args.method,
+                args.version,
+                args.interval,
+            )
+        )
     except KeyboardInterrupt:
         pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
