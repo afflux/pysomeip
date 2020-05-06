1f98d70e3077f4a0e00a4c795b6af5939ca553e0
Kjell Braden <afflux@pentabarf.de>
2020-05-06 13:52:10 +0200

    header: add missing type annotations to some options

--- src/someip/header.py
+++ src/someip/header.py
@@ -805,7 +805,7 @@ class AbstractIPOption(SOMEIPSDAbstractOption, typing.Generic[T]):
             proto=self.l4proto,
             flags=socket.AI_NUMERICHOST | socket.AI_NUMERICSERV,
         )
-        return addr[4]
+        return typing.cast(_T_SOCKNAME, addr[4])
 
 
 class EndpointOption(AbstractIPOption[T]):
@@ -865,13 +865,13 @@ class IPv4EndpointOption(AbstractIPv4Option, EndpointOption[ipaddress.IPv4Addres
 
 
 @SOMEIPSDOption.register
-class IPv4MulticastOption(AbstractIPv4Option, MulticastOption):
+class IPv4MulticastOption(AbstractIPv4Option, MulticastOption[ipaddress.IPv4Address]):
     type: typing.ClassVar[int] = 0x14
 
 
 @SOMEIPSDOption.register
 @dataclasses.dataclass(frozen=True)
-class IPv4SDEndpointOption(AbstractIPv4Option, SDEndpointOption):
+class IPv4SDEndpointOption(AbstractIPv4Option, SDEndpointOption[ipaddress.IPv4Address]):
     type: typing.ClassVar[int] = 0x24
 
 
@@ -881,13 +881,13 @@ class IPv6EndpointOption(AbstractIPv6Option, EndpointOption[ipaddress.IPv6Addres
 
 
 @SOMEIPSDOption.register
-class IPv6MulticastOption(AbstractIPv6Option, MulticastOption):
+class IPv6MulticastOption(AbstractIPv6Option, MulticastOption[ipaddress.IPv6Address]):
     type: typing.ClassVar[int] = 0x16
 
 
 @SOMEIPSDOption.register
 @dataclasses.dataclass(frozen=True)
-class IPv6SDEndpointOption(AbstractIPv6Option, SDEndpointOption):
+class IPv6SDEndpointOption(AbstractIPv6Option, SDEndpointOption[ipaddress.IPv6Address]):
     type: typing.ClassVar[int] = 0x26
 
 
